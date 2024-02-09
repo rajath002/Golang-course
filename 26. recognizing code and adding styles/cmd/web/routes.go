bab40a4c1af7a883d1224ad2d5b06e1b6af3e962
Rajath <77378371+rajath999@users.noreply.github.com>
2024-02-09 10:52:37 +0530

    39. Using chi for routing

--- 26. recognizing code and adding styles/cmd/web/routes.go
+++ 26. recognizing code and adding styles/cmd/web/routes.go
@@ -3,15 +3,21 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rajath002/RecognizingCodeAndAddingStyles/pkg/config"
 	"github.com/rajath002/RecognizingCodeAndAddingStyles/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.Logger)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
 	return mux
 }
