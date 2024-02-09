ff7a0d86c35058afd4a3e08bd6324d4a806c1ecd
Rajath <77378371+rajath999@users.noreply.github.com>
2024-02-09 11:05:30 +0530

    sec 4: 40. Developing our own middleware

--- 26. recognizing code and adding styles/cmd/web/routes.go
+++ 26. recognizing code and adding styles/cmd/web/routes.go
@@ -14,7 +14,8 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(middleware.Logger)
+	mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
--- 26. recognizing code and adding styles/go.mod
+++ 26. recognizing code and adding styles/go.mod
@@ -3,3 +3,5 @@ module github.com/rajath002/RecognizingCodeAndAddingStyles
 go 1.21.0
 
 require github.com/go-chi/chi/v5 v5.0.11
+
+require github.com/justinas/nosurf v1.1.1 // indirect
